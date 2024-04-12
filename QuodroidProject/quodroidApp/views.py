38cb6c0b1e9b9ef193bb3cd6ef8b7bd2e57949e7
Muhammed Yusayr <yusayrk1@gmail.com>
2024-04-13 02:03:03 +0530

    Uploading Code

--- QuodroidProject/quodroidApp/views.py
+++ QuodroidProject/quodroidApp/views.py
@@ -1,28 +1,57 @@
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import os
 
 @csrf_exempt
 def execute_test(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            tests = data.get('tests', [])
-            if tests:
-                first_test = tests[0]
-                title = first_test.get('title', '')
-                steps = first_test.get('steps', [])
-                # Return a JSON response
-                response_data = {
-                    'message': 'Received data successfully',
-                    'title': title,
-                    'steps': steps
+    if request.method == 'POST' or request.method == 'GET':
+        # Parse the JSON data from the request body
+        data = {
+            "tests": [
+                {
+                    "title": "Open google.com",
+                    "steps": [
+                        "Open Browser browser='chrome'",
+                        "Go To url='https://google.com'"
+                    ]
                 }
-                return JsonResponse(response_data, content_type='application/json',status=201)
-            else:
-                return JsonResponse({'error': 'No tests found'}, status=400)
-        except json.JSONDecodeError as e:
-            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+            ]
+        }
+
+        # Iterate over the 'tests' list correctly
+        for test in data['tests']:
+            title = test.get("title","")
+            robot_code = "***Settings***\n" +"Library\t" "SeleniumLibrary\n"+ "***Test Cases***\n" 
+            robot_code += title + "\n"
+            steps = test.get('steps',[])
+
+            for step in steps:
+                words = step.split()
+                command = " ".join(words[:2])
+                parameter = " ".join(words[2:])
+                modified_step = "\t" + command + "\t" + parameter.replace("'", "") + "\n"
+                robot_code += modified_step
+
+            # Write the robot_code to a file
+            with open('testfinal.robot', 'w') as file:
+                file.write(robot_code)
+
+            # Make a POST request to the same URL
+            # response = requests.post('http://127.0.0.1:8000/testai/tests/v1/execute', data=data)
+
+            # print(response.text)
+
+            with open('testfinal.robot', 'w') as file:
+                file.write(robot_code)
+
+            # Execute the Robot Framework test
+            os.system('robot testfinal.robot')
+
+        return HttpResponse(robot_code, content_type='text/plain')
+    else:
+        return HttpResponse('Invalid Object', status=405, content_type='application/json')
+
 
 
 
---
+++ QuodroidProject/testfinal.robot
@@ -0,0 +1,6 @@
+***Settings***
+Library	SeleniumLibrary
+***Test Cases***
+Open google.com
+	Open Browser	browser=chrome
+	Go To	url=https://google.com
